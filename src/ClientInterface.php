5dcf652a5be7d73f8b3f442f788fd3476f562cfb
Danil Pyatnitsev <danil@pyatnitsev.ru>
2017-10-21 19:02:40 +0300

    bump version 1.0

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -3,7 +3,7 @@ namespace Jilfond\Apirosreestr;
 
 interface ClientInterface {
 
-    const VERSION = '1.0-dev';
+    const VERSION = '1.0';
     const API_BASE_URL = 'https://apirosreestr.ru/api/';
 
 }
